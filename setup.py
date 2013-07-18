87a5cdec98cdbd27238565b3864fb379fb6bde26
Sylvain Viollon <sylvain@infrae.com>
2013-07-18 14:43:32 +0200

    Skeleton.

---
+++ setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2013 Infrae. All rights reserved.
+# See also LICENSE.txt
+
+from setuptools import setup, find_packages
+import os
+
+version = '1.0dev'
+
+tests_require = [
+    'infrae.wsgi [test]',
+    ]
+
+
+setup(name='infrae.fileupload',
+      version=version,
+      description="WSGI middleware to manage upload of files",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='silva cms file upload zope',
+      author='Infrae',
+      author_email='info@infrae.com',
+      url='http://infrae.com/products/silva',
+      license='BSD',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      namespace_packages=['infrae',],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        ],
+      tests_require = tests_require,
+      extras_require = {'test': tests_require},
+      )
---
+++ src/infrae/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2012  Infrae. All rights reserved.
+# See also LICENSE.txt
+# See http://peak.telecommunity.com/DevCenter/setuptools#namespace-packages
+try:
+    __import__('pkg_resources').declare_namespace(__name__)
+except ImportError:
+    from pkgutil import extend_path
+    __path__ = extend_path(__path__, __name__)
